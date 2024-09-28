5ec0561f5165650d5d456e82c1bece3280f4b6e4
Web-Dev-Learner <shribabe2@gmail.com>
2024-09-29 00:01:27 +0530

    updated salesoverview

--- src/components/sales/SalesOverviewChart.jsx
+++ src/components/sales/SalesOverviewChart.jsx
@@ -2,8 +2,6 @@ import { motion } from "framer-motion";
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
 import { useState } from "react";
 
-
-
 const monthlySalesData = [
 	{ month: "Jan", sales: 4000 },
 	{ month: "Feb", sales: 3000 },
@@ -14,32 +12,34 @@ const monthlySalesData = [
 	{ month: "Jul", sales: 7000 },
 ];
 
-
 const SalesOverviewChart = () => {
 	const [selectedTimeRange, setSelectedTimeRange] = useState("This Month");
 
 	return (
-    <motion.div
-    className='bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg rounded-xl p-6 border border-gray-700 mb-8'
-      initial={{ opacity: 0, y: 20 }}
+		<motion.div
+			className='bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg rounded-xl p-6 border border-gray-700 mb-8'
+			initial={{ opacity: 0, y: 20 }}
 			animate={{ opacity: 1, y: 0 }}
 			transition={{ delay: 0.2 }}
 		>
-<div className='flex items-center justify-between mb-6'>
-  <h2 className='text-xl ont-semibold text-gray-100'>Sales Overview</h2>
+			<div className='flex items-center justify-between mb-6'>
+				<h2 className='text-xl font-semibold text-gray-100'>Sales Overview</h2>
 
-  <select className='bg-gray-700 text-white rounded-md px-3 py-1 focus:outline-none ocus:ring-2
-  focus:ring-blue-500'
-   value={selectedTimeRange}
-   onChange={(e) => setSelectedTimeRange(e.target.value)}
-   >
-          <option>This Week</option>
-          <option>This Month</option>
+				<select
+					className='bg-gray-700 text-white rounded-md px-3 py-1 focus:outline-none focus:ring-2 
+          focus:ring-blue-500
+          '
+					value={selectedTimeRange}
+					onChange={(e) => setSelectedTimeRange(e.target.value)}
+				>
+					<option>This Week</option>
+					<option>This Month</option>
 					<option>This Quarter</option>
 					<option>This Year</option>
-   </select>
-</div>
-<div className='w-full h-80'>
+				</select>
+			</div>
+
+			<div className='w-full h-80'>
 				<ResponsiveContainer>
 					<AreaChart data={monthlySalesData}>
 						<CartesianGrid strokeDasharray='3 3' stroke='#374151' />
@@ -53,8 +53,7 @@ const SalesOverviewChart = () => {
 					</AreaChart>
 				</ResponsiveContainer>
 			</div>
-
-      </motion.div>
+		</motion.div>
 	);
 };
 export default SalesOverviewChart;
